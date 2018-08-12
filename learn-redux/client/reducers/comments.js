287974afbbc7a372b54225f9cf9eeef6985f22bb
caraclarke <cara.clarke8@gmail.com>
2018-08-12 19:30:07 -0400

    reducer updates

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -1,5 +1,34 @@
+function postComments(state = [], action) {
+  switch(action.type) {
+    case 'ADD_COMMENT':
+      // return new state with new comment
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT':
+      // need to return new state without deleted comment
+      return [
+        // from start to one we want to delete
+        ...state.slice(0, action.i),
+        // after deleted one to end
+        ...state.slice(action.i + 1)
+      ]
+    default:
+      return state;
+  }
+  return state;
+}
+
 function comments(state = [], action) {
-  console.log(state, action);
+  if (typeof action.postId !== 'undefined') {
+    return {
+      // take current state
+      ...state,
+      // overwrite this post with a new one
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
   return state;
 }
 
