ea160c12245ca383c89fecdba328bbe84bbe4294
caraclarke <cara.clarke8@gmail.com>
2018-08-12 13:02:16 -0400

    routing

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -2,8 +2,29 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+// react router dependencies
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+// components
+import Main from './components/Main';
+import PhotoGrid from './components/PhotoGrid';
+import Single from './components/Single';
+
 // import CSS
 import css from './styles/style.styl';
 
+// router
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+);
+
 // takes JSX and mounting point
-render(<p>hello</p>, document.getElementById('root'));
+render(
+  <Main />,
+  document.getElementById('root')
+);
