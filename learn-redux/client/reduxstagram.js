e3626bac8419eacc610ff2ac4745fda5a7566052
caraclarke <cara.clarke8@gmail.com>
2018-08-12 12:51:22 -0400

    getting started

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,9 @@
-// let's go!
+// this is like the run.js file in QB/QB
+import React from 'react';
+import { render } from 'react-dom';
+
+// import CSS
+import css from './styles/style.styl';
+
+// takes JSX and mounting point
+render(<p>hello</p>, document.getElementById('root'));
